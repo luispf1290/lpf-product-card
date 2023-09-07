fc0d47ed3d9940fb745550cc17b10dec53f026c7
Luis <luispia1290@gmail.com>
2023-09-06 19:23:52 -0600

    Version 0.0.1 lista

---
+++ test/components/ProductImage.test.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import renderer from 'react-test-renderer';
+import ProductCard, { ProductImage } from "../../src/components";
+import { product2 } from "../data/products";
+
+describe('ProductImage', () => {
+    test("debe de mostar el componente correctamente con la imagen perzonalizada", () => {
+       const wrapper = renderer.create(
+            <ProductImage img={'../hola.jpg'}/>
+       );
+       expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+
+    test("debe de mostrar el componente con la imagen del producto", () => {
+        const wrapper = renderer.create(
+            <ProductCard product={product2}>
+                {
+                    () => <ProductImage/>
+                }
+            </ProductCard>
+        )
+
+        expect( wrapper.toJSON()).toMatchSnapshot();
+    });
+    
+})
---
+++ test/data/products.ts
@@ -0,0 +1,10 @@
+export const product1 = {
+  id: '1',
+  title: 'CoffeMug-card',
+};
+
+export const product2 = {
+  id: '2',
+  title: 'CoffeMug-card',
+  img: '../../../public/coffee-mug.png',
+};
